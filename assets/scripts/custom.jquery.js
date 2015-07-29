fbbdb3b291590f4ff2ca0ca9a0afd91f73b8ba07
Emma Davis <emma@thewebbakery.co.uk>
2015-07-29 15:19:37 +0100

    Footer animation

--- assets/scripts/custom.jquery.js
+++ assets/scripts/custom.jquery.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
 
-	/**
-	* Infinately rotate screenshots from previous conference websites in the 'monster dude' screen on the home page
-	**/
+    window.requestAnimFrame = (function(){
+        return  window.requestAnimationFrame        ||
+                window.webkitRequestAnimationFrame  ||
+                window.mozRequestAnimationFrame     ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
 
-	$('.bxslider').bxSlider({
-		mode: 'vertical',
-		useCSS: false,
-		infiniteLoop: true,
-		hideControlOnEnd: true,
-		easing: 'easeOutElastic',
-		speed: 3000,
-		auto: true
-	});
+    /**
+    * Infinately rotate screenshots from previous conference websites in the 'monster dude' screen on the home page
+    **/
+
+    $('.bxslider').bxSlider({
+        mode: 'vertical',
+        useCSS: false,
+        infiniteLoop: true,
+        hideControlOnEnd: true,
+        easing: 'easeOutElastic',
+        speed: 3000,
+        auto: true
+    });
 
     /**
     * Rotate Buy Tickets background swirl on hover
@@ -22,45 +31,58 @@ $(document).ready(function() {
     var degree = 0;
 
     function animateSwirl(swirlID) {
-    	if(interval) {
-        	clearInterval(interval);
+        if(interval) {
+            clearInterval(interval);
         }
         interval = setInterval(function() {
-			$(swirlID)
-				.css('-webkit-transform', 'rotate('+degree+'deg)')
-				.css('-moz-transform', 'rotate('+degree+'deg)')
-				.css('-ms-transform', 'rotate('+degree+'deg)');
-				degree++; degree++; degree++;
-		}, 10);
+            $(swirlID)
+                .css('-webkit-transform', 'rotate('+degree+'deg)')
+                .css('-moz-transform', 'rotate('+degree+'deg)')
+                .css('-ms-transform', 'rotate('+degree+'deg)');
+                degree++; degree++; degree++;
+        }, 10);
     }
 
     $('#tickets-button').hover(
-    	function() {
-	        animateSwirl('#button-swirl');
-    	},
-    	function() {
-    		clearInterval(interval);
-    	}
+        function() {
+            animateSwirl('#button-swirl');
+        },
+        function() {
+            clearInterval(interval);
+        }
     );
 
     $('#buy--tickets-button').hover(
-    	function() {
-	        animateSwirl('#buy--tickets-button-swirl');
-    	},
-    	function() {
-    		clearInterval(interval);
-    	}
+        function() {
+            animateSwirl('#buy--tickets-button-swirl');
+        },
+        function() {
+            clearInterval(interval);
+        }
     );
 
     /**
-    *	In the footer, bring Monster Dude up from behind the bridge
-    *	as user scrolls down the page and footer comes into view
-    *	using parallax scrolling
+    *   In the footer, bring Monster Dude up from behind the bridge
+    *   as user scrolls down the page and footer comes into view
     **/
 
-    // Hide monster dude in footer fom view by loading outside the containing div on page load
-    // Check position of user's browser
-    // When the user's browser is XXXX px from the bottom of the page start parallax scroll
-    // When the footer is in full view stop the scroll
+    var monster = $('.footer-scene-monster');
+    var win = $(window);
+    var startScrollPos = $('.footer--sponsors').offset().top - 100;
+    
+    function scrollMonster() {
+        monster.animate({
+            bottom: 0,
+            opacity: 1
+        }, 1000).addClass('visible');
+    }
 
+    win.scroll(function() {
+        var pixelsScrolled = win.scrollTop();
+        if (pixelsScrolled >= startScrollPos) {
+            requestAnimFrame(scrollMonster);
+        }
+    });
+    
+    
 });
